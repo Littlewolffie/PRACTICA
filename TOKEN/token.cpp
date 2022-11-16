9bf744dfd47a97f2cf24c08fdff73fc5edb3f0ae
Alejandro Lobo Millan <117042742+Littlewolffie@users.noreply.github.com>
2022-11-16 14:21:59 +0100

    Update token.cpp

--- TOKEN/token.cpp
+++ TOKEN/token.cpp
@@ -1,43 +1,61 @@
 #include "token.hpp"
 
-explicit token::token(codi cod = NULLTOK) throw(error) 
+token::token(codi cod = NULLTOK) throw(error) 
 {
 	if(cod == CT_ENTERA or cod == CT_RACIONAL or cod == CT_REAL or cod == VARIABLE) {
 		throw error(IdentificadorIncorrecte);
 	} else {
 		code = cod;
+		
 	}
 }
 
-explicit token::token(int n) throw(error) 
+token::token(int n) throw(error) 
 {
 	tip.enter = n;
 	code = CT_ENTERA;
 	
 }
 
-explicit token::token(const racional & r) throw(error) 
+token::token(const racional & r) throw(error) 
 {
 	tip.frac = r;
 	code = CT_RACIONAL;
 }
 
-explicit token::token(double x) throw(error) 
+token::token(double x) throw(error) 
 {
 	tip.real = x;
 	code = CT_REAL;
 }
 
+bool comprova (string var){
+
+	bool cert = true;;
+	for (int i=0, i < var.size() and cert,++i){
+	// 65-90 97 122 95
+		if (var[i] < 65) cert = false;
+		if(var[i] < 97 and var[i] > 90 and var[i] != 95 ) cert = false;
+		if(var[i] > 122) cert = false;
+	}
+	return cert;
+
+}
+
 explicit token::token(const string & var_name) throw(error) 
 {
-	if(var_name == "unassign" or var_name == "e" or var_name == "sqrt" or var_name == "log" or var_name == "exp" or var_name == "evalf") {
-		throw error(IdentificadorIncorrecte);
-	} else if() {
+	if(var_name == "unassign" or var_name == "e" or var_name == "sqrt" or var_name == "log" 		or var_name == "exp" or var_name == "evalf") {
 	
-	} else {
+		throw error(IdentificadorIncorrecte);
+		
+	}else if(comprova(var_name)){
+	
 		tip.text = var_name;
 		code = VARIABLE;
-	}
+	} 
+
+	else throw error(IdentificadorIncorrecte);
+	
 }
 
 token::token(const token & t) throw(error) 
@@ -58,7 +76,7 @@ token::~token() throw()
 
 }
 
-codi token::tipus() const throw() 
+token::codi token::tipus() const throw() 
 {
 	return code;
 }
@@ -101,5 +119,47 @@ string token::identificador_variable() const throw(error)
 
 bool token::operator==(const token & t) const throw() 
 {
-
+	bool igual = false;
+	if (code == CT_ENTERA and t.code == CT_ENTERA and t.enter == enter){
+		cert = true;
+		
+	}
+	if (code == CT_REAL and t.code == CT_REAL and t.real == real){
+		cert = true;
+		
+	}
+	if (code == CT_RACIONAL and t.code == CT_RACIONAL and t.frac == frac){
+		cert = true;
+		
+	}
+	if (code == VARIABLE and t.code == VARIABLE and t.text == text){
+		cert = true;	
+	}
+	
+	return igual;
 }
+
+
+bool token::operator!=(const token & t) const throw() 
+{
+	return (not (*this == t));
+	
+}
+
+ bool token::operator>(const token & t) const throw(error)
+ {
+ 
+ 	if (code > t.code) return true;
+ 	else return false;
+ 
+ 
+ }
+ 
+  bool token::operator<(const token & t) const throw(error)
+ {
+ 
+ 	if (code < t.code) return true;
+ 	else return false;
+ 
+ 
+ }

72054eac9db542f7f4b015df0cf4eb1cbd354f83
Alejandro Lobo Millan <117042742+Littlewolffie@users.noreply.github.com>
2022-12-14 11:59:27 +0100

    Create math_io.cpp

---
+++ MATH.IO/math_io.cpp
@@ -0,0 +1,19 @@
+#include "math_io.hpp"
+
+
+/* Realitza l'anàlisi lèxic de la comanda en forma d'string s, la converteix
+en la seqüència de tokens i retorna aquesta seqüència. Es produeix un
+error si existeix algun error lèxic, és a dir, algun caràcter que
+impedeixi el reconeixement d'un nou token. */
+void math_io::scan(const string & s, list<token> & lt) throw(error){
+
+
+
+}
+
+// Converteix en un string la seqüència de tokens lt.
+string math_io::tostring(const list<token> & lt){
+
+    return (" ");
+
+}
